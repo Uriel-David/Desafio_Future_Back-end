b208a72e5f50e22142f9d6482ff62815a0df7e73
Uriel-David <uriel.david.qaa@gmail.com>
2021-12-05 23:18:29 +0000

    Conclusão e refinamento das funcionalidades da API.

--- src/controllers/filmesController.js
+++ src/controllers/filmesController.js
@@ -20,6 +20,48 @@ module.exports = {
         }
     },
 
+    async getFilmeCategoria(req, res) {
+
+        try {
+            const { CategoriaId } = req.params;
+            const filmes = await models.Filme.findAll({
+                include: [
+                    {
+                        model: models.Categoria,
+                        through: { attributes: [] },
+                        where: {
+                            id: CategoriaId,
+                        },
+                    }
+                ]
+            });
+            return res.status(200).send(filmes);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    },
+
+    async getFilmeGenero(req, res) {
+
+        try {
+            const { genero } = req.params;
+            const filmes = await models.Filme.findAll({
+                where: {
+                    genero: genero,
+                },
+                include: [
+                    {
+                        model: models.Categoria,
+                        through: { attributes: [] },
+                    }
+                ]
+            });
+            return res.status(200).send(filmes);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    },
+
     async postFilme(req, res) {
         try {
             const {categoria, ...data} = req.body;
@@ -53,9 +95,8 @@ module.exports = {
     },
 
     async deleteFilme(req, res) {
-
-        const { id } = req.params;
         try {
+            const { id } = req.params;
             const filmes = await models.Filme.findByPk(id);
             await filmes.destroy();
             return res.status(200).send(filmes);
--- src/routes/filmesRoute.js
+++ src/routes/filmesRoute.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const FilmeController = require('../controllers/filmesController');
 
 router.get('/filmes', FilmeController.getFilme);
+router.get('/filmes/categoria/:CategoriaId', FilmeController.getFilmeCategoria);
+router.get('/filmes/genero/:genero', FilmeController.getFilmeGenero);
 router.post('/filmes', FilmeController.postFilme);
 router.put('/filmes/:id', FilmeController.putFilme);
 router.delete('/filmes/:id', FilmeController.deleteFilme);
